137ecefec5077fdb5b1ea8303a94846111a5350a
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-11-24 14:51:44 +0100

    Add front and back folders in assets and templates

--- my_project_directory/webpack.config.js
+++ my_project_directory/webpack.config.js
@@ -20,6 +20,11 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+
+    .addEntry('front_scripts', './assets/front/script/index.ts')
+    .addStyleEntry('front_styles', './assets/front/style/index.scss') 
+
+
     .addEntry('script', './assets/script/index.ts')
     .addStyleEntry('style', './assets/style/index.scss') 
 
