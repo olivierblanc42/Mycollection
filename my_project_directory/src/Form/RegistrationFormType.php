052e34441d8dd010a799d776a9916cae644247d5
olivierblanc42 <olivierhumanbooster@gmail.com>
2026-01-06 14:32:54 +0100

    add: arguments for the form

--- my_project_directory/src/Form/RegistrationFormType.php
+++ my_project_directory/src/Form/RegistrationFormType.php
@@ -11,43 +11,122 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Regex;
+
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('userName')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('birthDate')
-
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'label' => 'Email',
+                    'attr' => [
+                        'placeholder' => 'Email'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer votre email.',
+                        ]),
+                    ]
                 ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+            )
+            ->add(
+                'userName',
+                TextType::class,
+                [
+                    'label' => 'Nom d\' utilisateur',
+                    'attr' => [
+                        'placeholder' => 'Nom d\'utilisateur'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer votre Nom d\'utilisateur.',
+                        ]),
+                    ]
+                ],
+
+
+            )
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer votre Nom.'
+
                     ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                    new Regex(
+                        pattern: '/^\p{L}+([ \'-]\p{L}+)*$/u',
+                        message: 'Lettres uniquement, avec espaces ou tirets entre les mots.'
+                    )
+                ]
+            ])
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'label' => 'Prénom',
+                    'attr' => [
+                        'placeholder' => 'Prénom'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer votre Prénom.',
+                        ]),
+                        new Regex(
+                            pattern: '/^[A-Za-zÀ-ÿ]+([ -][A-Za-zÀ-ÿ]+)*$/',
+                            message: 'Lettres uniquement, avec espaces ou tirets entre les mots.'
+                        )
+                    ]
+                ]
+            )
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Accepter les conditions d’utilisation',
+            'row_attr' => ['class' => 'agree'],
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos conditions.',
                     ]),
                 ],
             ])
+            ->add(
+                'plainPassword',
+                PasswordType::class,
+                [
+                    'label' => 'Mot de passe',
+
+                    // instead of being set onto the object directly,
+                    // this is read and encoded in the controller
+                    'mapped' => false,
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'placeholder' => 'Mot de passe'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer le mot de passe.',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+
+                    ],
+
+                ]
+            )
         ;
     }
 
