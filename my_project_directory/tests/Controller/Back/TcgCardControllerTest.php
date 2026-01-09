1a0740d812bb1bb019d468159bd54a4a4be3d4bc
olivierblanc42 <olivierhumanbooster@gmail.com>
2026-01-09 14:57:35 +0100

    create test for cintroller admin

---
+++ my_project_directory/tests/Controller/Back/TcgCardControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class TcgCardControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tcg/card');
+
+        self::assertResponseIsSuccessful();
+    }
+}
