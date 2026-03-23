18b49ee3791b28e2f3aca28f5176624e6f933d92
olivierblanc42 <olivierhumanbooster@gmail.com>
2026-03-23 10:00:59 +0100

    refactor(admin): use Repository in Controller

--- my_project_directory/src/Controller/Back/NetworkController.php
+++ my_project_directory/src/Controller/Back/NetworkController.php
@@ -5,14 +5,24 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\NetworkRepository;
+
+
 
 final class NetworkController extends AbstractController
 {
-    #[Route('/admin/network', name: 'app_network_admin')]
-    public function index(): Response
+    #[Route('/admin/network', name: 'app_network_admin', methods: ['GET'])]
+    public function index(
+        NetworkRepository $networkRepository
+
+    ): Response
     {
+           
+        $networks = $networkRepository->getQbAll()->getQuery()->getResult();
+
         return $this->render('back/network/index.html.twig', [
             'controller_name' => 'NetworkController',
+            'networks' => $networks
         ]);
     }
 }
--- my_project_directory/src/Controller/Back/TcgController.php
+++ my_project_directory/src/Controller/Back/TcgController.php
@@ -5,14 +5,23 @@ namespace App\Controller\Back;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\TcgRepository;
 
 final class TcgController extends AbstractController
 {
-    #[Route('/admin/tcg', name: 'app_tcg_admin')]
-    public function index(): Response
+    #[Route('/admin/tcg', name: 'app_tcg_admin', methods: ['GET'])]
+    public function index(
+    TcgRepository $tcgRepository
+
+    ): Response
     {
+
+        $tcgs = $tcgRepository->getQbAll()->getQuery()->getResult();
+
+
         return $this->render('back/tcg/index.html.twig', [
             'controller_name' => 'TcgController',
+            'tcg' => $tcgs,
         ]);
     }
 }
