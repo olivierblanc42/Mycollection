47d48758d7ed90c966b0c99d35cff5c53c333ac7
olivierblanc42 <olivierhumanbooster@gmail.com>
2026-03-31 09:54:02 +0200

    feat: integrate Type form in Type view

--- my_project_directory/src/Controller/Back/TypeController.php
+++ my_project_directory/src/Controller/Back/TypeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller\Back;
 
+use App\Form\TypeFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
+
+#[Route('/admin/type')]
 final class TypeController extends AbstractController
 {
-    #[Route('/admin/type', name: 'app_type_admin')]
-    public function index(): Response
+    #[Route('/', name: 'app_type_admin')]
+    public function index(
+        Request $request
+
+    ): Response
     {
+
+     $typeForm = $this->createForm(TypeFormType::class,null,[
+            'method' => 'GET',
+     ]);
+        $typeForm->handleRequest($request);
+
         return $this->render('back/type/index.html.twig', [
             'controller_name' => 'TypeController',
+            'typeForm'=> $typeForm->createView(),
         ]);
     }
 }
