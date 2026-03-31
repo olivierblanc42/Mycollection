cd828d68689161a582f0b5f0cca944e11207c8aa
olivierblanc42 <olivierhumanbooster@gmail.com>
2026-03-31 10:20:39 +0200

    feat: integrate Type form in Category view

--- my_project_directory/src/Controller/Back/CategoryController.php
+++ my_project_directory/src/Controller/Back/CategoryController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller\Back;
 
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
+
+#[Route('/admin/category')]
 final class CategoryController extends AbstractController
 {
-    #[Route('/admin/category', name: 'app_category_admin')]
-    public function index(): Response
+    #[Route('/', name: 'app_category_admin')]
+    public function index(
+        Request $request
+    ): Response
     {
+        $categoryForm = $this->createForm(CategoryType::class, null, [
+            'method' => 'GET',
+        ]);
+
+
         return $this->render('back/category/index.html.twig', [
             'controller_name' => 'CategoryController',
+            'categoryForm' => $categoryForm->createView(),
         ]);
     }
 }
